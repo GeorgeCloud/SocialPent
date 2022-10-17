d76f07c48add5ca2aa660dca740beeebdc2d6843
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2022-10-17 14:50:54 -0700

    fix: app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import bcrypt
 
 app = Flask(__name__)
 
-app.secret_key = b'something'
+app.secret_key = bytes(environ.get("SECRET_KEY"), 'utf-8')
 
 uri = environ.get('MONGODB_URI', 'mongodb://localhost:27017/reveal')
 client = MongoClient(uri)
