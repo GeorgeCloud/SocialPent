11e43b195cf8defb9ad07f9300bb180fdb3a61a1
GeorgeCloud <net.george.sky@gmail.com>
2023-01-05 02:35:59 -0800

    add todo for event table

--- helpers/events_helper.py
+++ helpers/events_helper.py
@@ -2,22 +2,28 @@ def cleanup_api_data(api_events):
     events = []
 
     for event in api_events['_embedded']['events']:
-        e_info  = event['dates']['start']
-        # e_price = event['priceRanges'][0]
+        # e_price  = event['priceRanges'][0]
+        e_info     = event['dates']['start']
         venue_info = event['_embedded']['venues'][0]
+        event      = {
+                        'name'       : event['name'],
+                        'type'       : event['type'],
+                        'date'       : e_info['localDate'],
+                        'coordinates': venue_info['location'],
+                        'image_path' : event['images'][0]['url'],
+                        'url'        : event['url'],
 
-        events.append({
-            'name'       : event['name'],
-            'type'       : event['type'],
-            'date'       : e_info['localDate'],
-            'coordinates': venue_info['location'],
-            'image_path' : event['images'][0]['url'],
-            'url'        : event['url']
+                        # API returns events with missing data
+                        # 'info'       : event['info'],
+                        # 'time'       : e_info['localTime'],
+                        # 'address'    : venue_info['address']['line1'],
+                        # 'price'      : {'min': e_price['min'], 'max': e_price['max']},
+        }
+        events.append(event)
 
-            # 'info'       : event['info'],
-            # 'time'       : e_info['localTime'],
-            # 'address'    : venue_info['address']['line1'],  # Some event data has no address
-            # 'price'      : {'min': e_price['min'], 'max': e_price['max']},
-        })
+        # TODO: Ensure inserting is unique by ID given by TICKETMASTER API
+        # Implement background worker
+
+        # db.events.insert_one(event)
 
     return events
