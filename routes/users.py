5dd10374a76cdd61770bb389e359b918111884a9
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2023-01-05 13:10:55 -0800

    add: css & view post route & edit post route

--- routes/users.py
+++ routes/users.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, session, request
 from datetime import datetime
 from extensions import *
 from db import db
+from bson import ObjectId
 import bcrypt
 
 users_bp = Blueprint('users_bp', __name__, template_folder='templates')
@@ -42,3 +43,17 @@ def create_post(username):
     db.posts.insert_one(post)
 
     return redirect(url_for('users_bp.view_profile', username=username))
+
+@users_bp.route('/<username>/posts/<post_id>', methods=['GET'])
+@login_required
+def view_post(username, post_id):
+    # Ensure post_id belongs to username
+    post = db.posts.find_one({'_id': ObjectId(post_id)})
+    return render_template('show_post.html', username=username, post=post)
+
+@users_bp.route('/<username>/posts/<post_id>/edit', methods=['GET'])
+@login_required
+def edit_post(username, post_id):
+    user = db.users.find_one({'username': username})
+    post = db.posts.find_one({ '$and': [ {'_id': ObjectId(post_id) }, { 'user_id': user['_id'] }] })
+    return render_template('post_edit.html', username=username, post=post)
