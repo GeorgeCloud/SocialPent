569aca46927c30a12409237f2b9dea791aaaebe1
GeorgeCloud <net.george.sky@gmail.com>
2023-01-05 12:00:05 -0800

    style homepage

--- helpers/events_helper.py
+++ helpers/events_helper.py
@@ -2,7 +2,7 @@ def cleanup_api_data(api_events):
     events = []
 
     for event in api_events['_embedded']['events']:
-        # e_price  = event['priceRanges'][0]
+        e_price    = event.get('priceRanges')
         e_info     = event['dates']['start']
         venue_info = event['_embedded']['venues'][0]
         event      = {
@@ -14,10 +14,10 @@ def cleanup_api_data(api_events):
                         'url'        : event['url'],
 
                         # API returns events with missing data
-                        # 'info'       : event['info'],
-                        # 'time'       : e_info['localTime'],
-                        # 'address'    : venue_info['address']['line1'],
-                        # 'price'      : {'min': e_price['min'], 'max': e_price['max']},
+                        'info'       : event.get('info'),
+                        'time'       : e_info,
+                        'address'    : venue_info.get('address').get('line1'),
+                        'price'      : {'min': e_price[0].get('min'), 'max': e_price[0].get('max')} if e_price else None,
         }
         events.append(event)
 
